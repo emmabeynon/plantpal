6c43b989c8c2964527126d2e2e7dcaea7fa3a17b
Emma Beynon <emma.beynon@digital.cabinet-office.gov.uk>
2019-08-16 16:59:54 +0100

    Switch DB to use Postgres
    Also use dotenv to store DB secrets.

--- plantpal/settings.py
+++ plantpal/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'plantpal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('POSTGRES_DB_NAME'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
