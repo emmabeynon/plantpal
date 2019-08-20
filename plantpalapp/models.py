001f8c494295ce8daca0851899b6f7a6ed7678cc
Emma Beynon <emma.beynon@digital.cabinet-office.gov.uk>
2019-08-20 16:40:31 +0100

    Make Plant purchased_date not null
    We don't always know what the purchased date is for a plant, so we make this
    optional.

--- plantpalapp/models.py
+++ plantpalapp/models.py
@@ -3,4 +3,4 @@ from django.db import models
 class Plant(models.Model):
     name = models.CharField(max_length=200)
     location = models.CharField(max_length=100)
-    purchased_date = models.DateField()
+    purchased_date = models.DateField(blank=True,null=True)
