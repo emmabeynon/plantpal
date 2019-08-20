ef0450a3d205bf5fa3893a16917b6b8360878684
Emma Beynon <emma.beynon@digital.cabinet-office.gov.uk>
2019-08-20 16:43:46 +0100

    Add #str to Plant model
    This makes a the representation of a Plant object a bit more helpful by
    returning its name.

--- plantpalapp/models.py
+++ plantpalapp/models.py
@@ -4,3 +4,6 @@ class Plant(models.Model):
     name = models.CharField(max_length=200)
     location = models.CharField(max_length=100)
     purchased_date = models.DateField(blank=True,null=True)
+
+    def __str__(self):
+        return self.name
